038107560d0a4ed5e4fe03d59ce4c3b640896e68
Sevilla <sevillas2@nih.gov>
2019-12-30 18:01:22 -0500

    remove files tracked elsewhere

--- testing/training-feature-classifiers/config.sh
+++ testing/training-feature-classifiers/config.sh
@@ -2,11 +2,9 @@
 #Create a new trained classifer, using the SILVA DB. The most updated classifer
 #available for the QIIME Version 2017.11 is SILVA DB X.
 
-#dir
 parent_dir="/DCEG/Projects/Microbiome/CGR_MB/MicroBiome/sc_scripts_qiime2_pipeline/dev/training-feature-classifiers/"
 training_dir="silva_132_99_16S_20191016/"
 ref_dir="/DCEG/Projects/Microbiome/CGR_MB/MicroBiome/sc_scripts_qiime2_pipeline/working/Resources/Silva_132_release/"
-#downloaded from:
 
 fasta_file_path="SILVA_132_QIIME_release/rep_set/rep_set_16S_only/99/silva_132_99_16S.fna" #fasta file (using reference sequences clustered at 99% similarity)
 tax_file_path="SILVA_132_QIIME_release/taxonomy/16S_only/99/taxonomy_all_levels.tsv" #must be TSV format
